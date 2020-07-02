32cda9fea145cb838a761ae346b0647095560235
Victor Karangwa <victorkarangwa4@gmail.com>
2020-07-02 16:42:00 +0200

    Updates

---
+++ build/precache-manifest.3c06a647b9f8ba6cba28b71e67c05a59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b5694264c52aa4f0a7e060cfd83bbb",
+    "url": "/reactComingSoon_template/index.html"
+  },
+  {
+    "revision": "70bea8582fbcc3b3b70a",
+    "url": "/reactComingSoon_template/static/css/main.278b60cb.chunk.css"
+  },
+  {
+    "revision": "477595480ddec40e490f",
+    "url": "/reactComingSoon_template/static/js/2.33b11cdb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactComingSoon_template/static/js/2.33b11cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70bea8582fbcc3b3b70a",
+    "url": "/reactComingSoon_template/static/js/main.9551bcec.chunk.js"
+  },
+  {
+    "revision": "69cc2a83dc594ba2c956",
+    "url": "/reactComingSoon_template/static/js/runtime-main.337b645c.js"
+  },
+  {
+    "revision": "d6dab34f11f18bfbe92affb3ceb4efb5",
+    "url": "/reactComingSoon_template/static/media/thecampusLogo.d6dab34f.jpeg"
+  }
+]);
